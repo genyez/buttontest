9fc8dd2e686a9f611c4e657f1a373c7c2f384a2b
laoweimin <laoweimin@corp.netease.com>
2022-03-11 11:00:05 +0800

    timer

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # @Author: laoweimin@corp.netease.com
 # @Time: 2022/3/11 10:23
 from PyQt5.QtWidgets import QPushButton, QLabel, QWidget, QHBoxLayout, QVBoxLayout
-from PyQt5.Qt import QApplication, QPixmap, QImage, QSize
+from PyQt5.Qt import QApplication, QPixmap, QImage, QSize, QTimer
 
 
 class TestPoint(object):
@@ -12,14 +12,17 @@ class TestPoint(object):
 	NAME = "测试"
 	def __init__(self, x, y, key, parent):
 		super(TestPoint, self).__init__()
+		self.parent = parent
 		self.button = QPushButton(self.NAME)
-		self.button.setParent(parent)
+		self.button.setParent(parent.window)
 		self.button.clicked.connect(self.onClick)
 		self.button.resize(self.SIZE)
 		self.button.move(x, y)
 		self.key = key
 
 	def onClick(self):
+		self.parent.currenttime = 60
+		self.parent.timer.start()
 		print(self.key)
 
 
@@ -40,6 +43,10 @@ class TestJob(object):
 	def __init__(self):
 		super(TestJob, self).__init__()
 		self.testpoints = []
+		self.timer = QTimer()
+		self.timer.setInterval(1000)
+		self.timer.timeout.connect(self.onTimeout)
+		self.currenttime = 0
 
 	def initUI(self):
 		self.window = QWidget()
@@ -53,13 +60,36 @@ class TestJob(object):
 		image.load("map.jpg")
 		self.label.setPixmap(QPixmap.fromImage(image))
 
+		hlayout = QHBoxLayout()
+
+		self.resttime = QLabel()
+		self.stop = QPushButton("停止")
+		self.stop.clicked.connect(self.onStop)
+
+		hlayout.addWidget(QLabel("剩余:"))
+		hlayout.addWidget(self.resttime)
+		hlayout.addWidget(self.stop)
+		layout.addLayout(hlayout)
 		self.initTestPoints()
 
 		self.window.show()
 
+	def onTimeout(self):
+		if self.currenttime > 0:
+			self.currenttime -= 1
+			self.resttime.setText(str(self.currenttime))
+		else:
+			self.timer.stop()
+
+	def onStop(self):
+		self.timer.stop()
+		self.currenttime = 0
+		self.resttime.setText(str(self.currenttime))
+		print("stop")
+
 	def initTestPoints(self):
 		for p in testpoints:
-			self.testpoints.append(TestPoint(p["x"],p["y"],p["key"], self.window))
+			self.testpoints.append(TestPoint(p["x"],p["y"],p["key"], self))
 
 
 def main():
